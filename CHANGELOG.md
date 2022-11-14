72f2491aeae570129cfd45f4e2baddb1bb6372b5
zied-elouaer <zied.elouaer@mineiros.io>
2022-11-14 14:45:26 +0100

    chore: update module from template

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+- BREAKING CHANGE: Remove support for Terraform Google provider version `< 4.0`.
+
 ## [0.0.12]
 
 ### Changed
--- test/unit-disabled/main.tf
+++ test/unit-disabled/main.tf
@@ -4,25 +4,6 @@
 # The purpose is to verify no resources are created when the module is disabled.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-variable "gcp_project" {
-  type        = string
-  description = "(Required) The ID of the project in which the resource belongs."
-}
-
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "google" {
-  project = var.gcp_project
-}
-
-# DO NOT RENAME MODULE NAME
 module "test" {
   source = "../.."
 
@@ -71,9 +52,3 @@ module "test" {
 
   # add only required arguments and no optional arguments
 }
-
-# outputs generate non-idempotent terraform plans so we disable them for now unless we need them.
-# output "all" {
-#   description = "All outputs of the module."
-#   value       = module.test
-# }
