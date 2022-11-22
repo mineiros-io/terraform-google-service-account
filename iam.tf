822fdaa4e25cf17e2225e5fa086a6e0fb2a6f57e
Marius Tolzmann <marius@mineiros.io>
2022-11-22 20:52:08 +0100

    funky

--- iam.tf
+++ iam.tf
@@ -1,7 +1,13 @@
 locals {
-  iam_map = { for idx, iam in var.iam :
+  iam_mapx = { for idx, iam in var.iam :
     try(iam._key, iam.role) => iam
   }
+
+  iam_map = { for idx, iam in var.iam :
+    try(iam._key, iam.role) => idx
+  }
+
+  iam = values(local.iam_mapx)
 }
 
 module "iam" {
@@ -14,21 +20,21 @@ module "iam" {
 
   service_account_id = try(google_service_account.service_account[0].name, null)
 
-  role                 = each.value.role
-  members              = try(each.value.members, [])
+  role                 = local.iam[each.value].role
+  members              = try(local.iam[each.value].members, [])
   computed_members_map = var.computed_members_map
-  authoritative        = try(each.value.authoritative, true)
+  authoritative        = try(local.iam[each.value].authoritative, true)
 }
 
-module "policy_bindings" {
-  source = "github.com/mineiros-io/terraform-google-service-account-iam.git?ref=v0.1.0"
-
-  count = var.policy_bindings != null ? 1 : 0
-
-  module_enabled    = var.module_enabled
-  module_depends_on = var.module_depends_on
-
-  service_account_id   = try(google_service_account.service_account[0].name, null)
-  policy_bindings      = var.policy_bindings
-  computed_members_map = var.computed_members_map
-}
+# module "policy_bindings" {
+#   source = "github.com/mineiros-io/terraform-google-service-account-iam.git?ref=v0.1.0"
+#
+#   count = var.policy_bindings != null ? 1 : 0
+#
+#   module_enabled    = var.module_enabled
+#   module_depends_on = var.module_depends_on
+#
+#   service_account_id   = try(google_service_account.service_account[0].name, null)
+#   policy_bindings      = var.policy_bindings
+#   computed_members_map = var.computed_members_map
+# }
