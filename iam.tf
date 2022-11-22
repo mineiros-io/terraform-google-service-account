8ede87a7a889dd2d53df432e2a34d33984657397
Marius Tolzmann <marius@mineiros.io>
2022-11-22 21:49:16 +0100

    fix: computed values in iam bindings

--- iam.tf
+++ iam.tf
@@ -10,24 +10,28 @@ locals {
   iam = concat(local.iam_role, local.iam_roles)
 
   iam_map = { for idx, iam in local.iam :
-    try(iam._key, iam.role) => idx
+    try(iam._key, iam.role) => iam
   }
+
+  # hack: type-safe ternary
+  iam_for_each = [local.iam_map, {}][var.policy_bindings == null ? 0 : 1]
 }
 
 module "iam" {
   source = "github.com/mineiros-io/terraform-google-service-account-iam.git?ref=v0.1.0"
 
-  for_each = var.policy_bindings == null ? local.iam_map : {}
+  for_each = local.iam_for_each
 
   module_enabled    = var.module_enabled
   module_depends_on = var.module_depends_on
 
   service_account_id   = try(google_service_account.service_account[0].name, null)
-  role                 = local.iam[each.value].role
-  members              = try(local.iam[each.value].members, [])
+  role                 = each.value.role
+  members              = try(each.value.members, [])
   computed_members_map = var.computed_members_map
-  authoritative        = try(local.iam[each.value].authoritative, true)
+  authoritative        = try(each.value.authoritative, true)
 }
+
 module "policy_bindings" {
   source = "github.com/mineiros-io/terraform-google-service-account-iam.git?ref=v0.1.0"
 
--- test/unit-complete/main.tf
+++ test/unit-complete/main.tf
@@ -16,103 +16,131 @@ module "test" {
   module_enabled = true
 
   # add all required arguments
-  account_id = "unit-complete"
+  account_id = "unit-complete-1-${local.random_suffix}"
 
   # add all optional arguments that create additional resources
   iam = [
+    ### authoritative tests
+    # when authoritative is true we do not need computed map
     {
-      role    = "roles/browser"
-      members = ["domain:example-domain"]
+      role = "roles/dummyRole0"
+      members = [
+        "serviceAccount:${module.test-sa.service_account.email}",
+      ]
+      authoritative = true
     },
     {
-      role          = "roles/viewer"
-      members       = ["domain:example-domain", "principalSet:example"]
+      roles = [
+        "roles/dummyRole0.1",
+        "roles/dummyRole0.2",
+      ]
+      members = [
+        "serviceAccount:${module.test-sa.service_account.email}",
+      ]
+      authoritative = true
+    },
+    {
+      role          = "roles/dummyRole1"
+      authoritative = true
+    },
+    {
+      roles = [
+        "roles/dummyRole1.1",
+        "roles/dummyRole1.2",
+      ]
+      authoritative = true
+    },
+
+    ### authoritative tests
+    # default
+    {
+      role = "roles/dummyRole2"
+      members = [
+        "serviceAccount:${module.test-sa.service_account.email}",
+      ]
+    },
+    {
+      roles = [
+        "roles/dummyRole2.0",
+        "roles/dummyRole2.1",
+      ]
+      members = [
+        "serviceAccount:${module.test-sa.service_account.email}",
+      ]
+    },
+    {
+      role = "roles/dummyRole3"
+    },
+    {
+      roles = [
+        "roles/dummyRole3.1",
+        "roles/dummyRole3.2",
+      ]
+    },
+
+    ### non authoritative tests
+    {
+      role = "roles/dummyRole4"
+      members = [
+        "computed:myserviceaccount",
+      ]
       authoritative = false
     },
     {
-      roles   = ["roles/editor", "roles/creator"]
-      members = ["computed:computed_sa"]
-    }
-  ]
-  computed_members_map = {
-    myserviceaccount = "serviceAccount:${module.test-sa.service_account.email}"
-  }
-  projects_access = [
+      roles = [
+        "roles/dummyRole4.1",
+        "roles/dummyRole4.2",
+      ]
+      members = [
+        "computed:myserviceaccount",
+      ]
+      authoritative = false
+    },
     {
-      project = "terraform-service-catalog"
-      roles   = ["roles/viewer"]
-    }
-  ]
-  folders_access = [
+      role          = "roles/dummyRole5"
+      authoritative = false
+    },
     {
-      folder = "692674674684"
-      roles  = ["roles/viewer"]
-    }
-  ]
-  organization_access = [
-    {
-      org_id = "52211381732"
-      roles  = ["roles/viewer"]
+      roles = [
+        "roles/dummyRole5.1",
+        "roles/dummyRole5.2",
+      ]
+      authoritative = false
     }
   ]
 
-  # add most/all other optional arguments
+  computed_members_map = {
+    myserviceaccount = "serviceAccount:${module.test-sa.service_account.email}"
+  }
+
   display_name = "Unit Complete Test Service Account"
   description  = "A service account created by a unit-tests in https://github.com/mineiros-io/terraform-google-service-account."
   project      = var.gcp_project
 
-  # module_timeouts = {
-  #   google_monitoring_notification_channel = {
-  #     create = "10m"
-  #     update = "10m"
-  #     delete = "10m"
-  #   }
-  # }
-
   module_depends_on = ["nothing"]
 }
 
-module "testPolicy" {
+module "test2" {
   source = "../.."
 
   module_enabled = true
 
-  # add all required arguments
-  account_id = "unit-complete"
+  account_id = "unit-complete-2-${local.random_suffix}"
 
-  # add all optional arguments that create additional resources
   policy_bindings = [
     {
-      role    = "roles/iam.serviceAccountUser"
-      members = ["group:terraform-tests@mineiros.io"]
-      condition = {
-        title       = "expires_after_2030_12_31"
-        description = "Expiring at midnight of 2030-12-31"
-        expression  = "request.time < timestamp(\"2030-01-01T00:00:00Z\")"
-      }
-    }
-  ]
-}
-
-module "testPolicyIAM" {
-  source = "../.."
-
-  module_enabled = true
-
-  # add all required arguments
-  account_id = "unit-complete"
-
-  # add all optional arguments that create additional resources
-  iam = [
+      role = "roles/dummyRole0"
+      members = [
+        "computed:myserviceaccount",
+        "serviceAccount:${module.test-sa.service_account.email}",
+      ]
+      authoritative = false
+    },
     {
-      role    = "roles/storage.objectViewer"
-      members = ["projectOwner:example-project"]
-    }
-  ]
-  policy_bindings = [
-    {
-      role    = "roles/iam.serviceAccountUser"
-      members = ["group:terraform-tests@mineiros.io"]
+      role = "roles/dummyRole1"
+      members = [
+        "group:terraform-tests@mineiros.io",
+      ]
       condition = {
         title       = "expires_after_2030_12_31"
         description = "Expiring at midnight of 2030-12-31"
